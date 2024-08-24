b8eff772ace1b6c44fb625f13802d36df8285350
Jarvis <monuavip@gmail.com>
2024-08-24 16:34:03 +0300

    init commit

---
+++ 6 mod/multithreading-main/06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1631(Factory method pattern)/src/task1631/common/JpgReader.java
@@ -0,0 +1,8 @@
+package task1631.common;
+
+    public class JpgReader implements ImageReader {
+        public void read() {
+            System.out.println("Reading JPG image");
+        }
+    }
+
---
+++ 6 mod/multithreading-main/06 Знакомство с нитями Thread, Runnable, start, join, interrupt, sleep, ФАБРИКА (Мультипоточка-0)/task1631(Factory method pattern)/src/task1631/common/PngReader.java
@@ -0,0 +1,7 @@
+package task1631.common;
+
+public class PngReader implements ImageReader {
+    public void read() {
+        System.out.println("Reading PNG image");
+    }
+}
